3ead16d81efa34a38a1afb594a4a0121e56055f6
Skola Rajak QA <skolarajak.qa@gmail.com>
2020-12-05 18:04:13 +0100

    Add project files.

---
+++ Domaci_Cas23/BasicStats.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Domaci_Cas23
+{
+    class BasicStats
+    {
+
+        public static int GetLowest(List<int> Numbers)
+        {
+            int LowestNumber = Numbers.First();
+
+            foreach(int Number in Numbers)
+            {
+                if (Number < LowestNumber)
+                {
+                    LowestNumber = Number;
+                }
+            }
+
+            return LowestNumber;
+        }
+
+        public static int GetHighest(List<int> Numbers)
+        {
+            int HighestNumber = 0;
+
+            foreach(int Number in Numbers)
+            {
+                if (Number > HighestNumber)
+                {
+                    HighestNumber = Number;
+                }
+            }
+
+            return HighestNumber;
+        }
+
+        public static int GetSum(List<int> Numbers)
+        {
+            int sum = 0;
+            foreach(int Number in Numbers)
+            {
+                sum += Number;
+            }
+
+            return sum;
+        }
+        public static double GetAverage(List<int> Numbers)
+        {
+            return (double) GetSum(Numbers) / (double) Numbers.Count();
+        }
+
+    }
+}
---
+++ Domaci_Cas23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domaci_Cas23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>();
+            string UserInput;
+
+            do
+            {
+                Console.Write("Unesite broj (ostaviti prazno za kraj rada) >");
+                UserInput = Console.ReadLine();
+
+                if (UserInput != "")
+                {
+                    Numbers.Add(Convert.ToInt32(UserInput));
+                }
+
+            } while (UserInput != "");
+
+            Console.WriteLine("GetLowest = {0}", BasicStats.GetLowest(Numbers));
+            Console.WriteLine("GetHighest = {0}", BasicStats.GetHighest(Numbers));
+            Console.WriteLine("GetAverage = {0}", BasicStats.GetAverage(Numbers));
+            Console.WriteLine("GetSum = {0}", BasicStats.GetSum(Numbers));
+            Console.ReadKey();
+        }
+    }
+}
